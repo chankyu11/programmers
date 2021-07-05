fdb79a74b5d1f0daea45be52b94d5a15c92e0f61
CHANKYU\lck11 <lck1130@naver.com>
2021-07-05 14:28:41 +0900

    level2

--- 소수만들기.py
+++ 소수만들기.py
@@ -5,13 +5,15 @@ from itertools import combinations
 
 def solution(nums):
     answer = 0
-    cmb = list(combinations(nums, 3))
-    for number_set in cmb:
-        current_number = sum(number_set)
-        current_number_prime = True
-        for i in range(2, int(current_number ** 0.5) + 1):
-            if current_number % i == 0:
-                current_number_prime = False
-        if current_number_prime:
+    cb = list(combinations(nums, 3))
+    for i in cb:
+        cb_sum = sum(i)
+        prime = True
+        for j in range(2, int(cb_sum ** 0.5)+1):
+            if cb_sum % j == 0:
+                prime = False
+                
+        if prime:
             answer += 1
+    
     return answer
