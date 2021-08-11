311d01407faaa20c6dc7ba67e93ace9d333c7536
Lee Chankyu <65216988+chankyu11@users.noreply.github.com>
2021-08-11 12:21:19 +0900

    Delete [1차]다트게임.py

--- [1차]다트게임.py
+++ [1차]다트게임.py
@@ -2,31 +2,31 @@
 
 def solution(dartResult):
     n = ''
-    res = []
-   
+    score = []
+    
     for i in dartResult:
         if i.isdigit():
             n += i
             print(n)
         elif i == 'S':
             n = int(n)**1
-            res.append(n)
+            score.append(n)
             n = ''
         elif i == 'D':
             n = int(n)**2
-            res.append(n)
+            score.append(n)
             n = ''
         elif i == 'T':
             n = int(n)**3
-            res.append(n)
+            score.append(n)
             n = ''
         elif i == '*':
-            if len(res) > 1:
-                res[-2] = res[-2] * 2
-                res[-1] = res[-1] * 2
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
             else:
-                res[-1] = res[-1] * 2
+                score[-1] = score[-1] * 2
         elif i == '#':
-            res[-1] = res[-1] * -1
-
-    return sum(res)
+            score[-1] = score[-1] * -1
+    
+    return sum(score)
